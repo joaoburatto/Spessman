4069f64e8c2d0d3abfac02e4bb0192682a9ffb0c
João Buratto <joaoburattoc@gmail.com>
2021-02-23 19:35:38 -0300

    Basic movement and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform camera;
+    public Vector3 positionOffset;
+    public Vector3 rotationOffset;
+    
+    private void Update()
+    {
+        // Set camera to the target position 
+        transform.position = target.position - positionOffset;
+        
+        // Rotation
+        Vector3 newRotation = rotationOffset;
+
+        // Mouse input for rotation
+        Vector3 input = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+        newRotation += input;
+
+        // Mouse input for zoom
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        camera.position += camera.forward * zoom;
+        
+        // Set camera rotation
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + newRotation);
+    }
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,17 +9,31 @@ public class CharacterMovement : MonoBehaviour
     public float movementSpeed;
     
     public Vector3 currentMovement = Vector3.zero;
-    
+
+    // Mouse looking target
+    public Transform target;
+
     private void Update()
     {
+        // Input WASD
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        // Joins movement input
         Vector3 newMovementInput = new Vector3(horizontal, 0, vertical);
-        currentMovement = Vector3.Lerp(currentMovement, newMovementInput, Time.deltaTime * movementSpeed);
+        
+        // Interpolates current movement to new movement
+        currentMovement = Vector3.Lerp(currentMovement, newMovementInput * movementSpeed, Time.deltaTime);
 
+        // if there's no input we interpolate to zero
+        if (newMovementInput.magnitude == 0)
+        {
+            currentMovement = Vector3.Lerp(currentMovement, Vector3.zero, Time.deltaTime * 4);
+        }
+
+        // Actually move
         controller.Move(currentMovement);
-
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(currentMovement), 360);
+        // Look at target
+        transform.LookAt(transform.position + currentMovement);
     }
 }
