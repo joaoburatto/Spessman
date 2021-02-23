4c482d49d746ad5e696921fdd1da3329241957e2
João Buratto <joaoburattoc@gmail.com>
2021-02-22 23:09:43 -0300

    Basic assets

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float movementSpeed;
+    
+    public Vector3 currentMovement = Vector3.zero;
+    
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 newMovementInput = new Vector3(horizontal, 0, vertical);
+        currentMovement = Vector3.Lerp(currentMovement, newMovementInput, Time.deltaTime * movementSpeed);
+
+        controller.Move(currentMovement);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(currentMovement), 360);
+    }
+}
