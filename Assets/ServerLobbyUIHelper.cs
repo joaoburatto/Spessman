ceb6e58d9d7135ffb7a37557813a74cfae6ca5a2
João Buratto <joaoburattoc@gmail.com>
2021-02-27 13:25:54 -0300

    Outline shader fix

--- Assets/ServerLobbyUIHelper.cs
+++ Assets/ServerLobbyUIHelper.cs
@@ -11,22 +11,49 @@ public class ServerLobbyUIHelper : NetworkBehaviour
     public Button spawnButton;
 
     public Animator animator;
+    
+    private NetworkManager networkManager;
 
     private void Start()
     {
+        networkManager = NetworkManager.singleton;
+            
         spawnButton.onClick.AddListener( delegate 
         { 
             CmdSpawnPlayer(); 
         });
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) { FadeUI();}
+    }
+
     public void FadeUI(bool state)
     {
         animator.SetBool("Fade", state);
     }
+    
+    public void FadeUI()
+    {
+        bool state = animator.GetBool("Fade");
+        animator.SetBool("Fade", !state);
+    }
+    
     [Command(ignoreAuthority = true)]
     public void CmdSpawnPlayer(NetworkConnectionToClient sender = null)
     {
         NetworkManager.singleton.SpawnPlayer(sender);
+    }
+    
+    public void OnDisconnectButtonPressed() 
+    {
+        if (isServer) networkManager.StopHost();
+        if (isClient) networkManager.StopClient();
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
     }
 }
