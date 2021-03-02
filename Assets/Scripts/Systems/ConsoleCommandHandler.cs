78a00fcc4141059b549b9310383e4d53f28b9eb6
João Buratto <joaoburattoc@gmail.com>
2021-03-02 12:58:59 -0300

    Simple Console

---
+++ Assets/Scripts/Systems/ConsoleCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Spessman
+{
+    public class ConsoleCommandHandler : NetworkBehaviour
+    {
+        // TODO: Add a dropdown for commands that can have autocomplete
+        public static ConsoleCommandHandler singleton { get; private set; }
+        
+        private void Awake()
+        {
+            if (singleton != null) Destroy(gameObject);
+            singleton = this;
+        }
+        
+        public void Command(string command)
+        {
+            NetworkManager networkManager = NetworkManager.singleton;
+            switch (command)
+            {
+                case "quit":
+                    Application.Quit();
+                    break;
+                case "disconnect":
+                    if (isServer) networkManager.StopHost();
+                    if (isClient) networkManager.StopClient();
+                    break;
+            }
+        }
+    }
+}
