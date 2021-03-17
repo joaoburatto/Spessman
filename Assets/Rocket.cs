0b860c4b5b66d821a761bc98289289d05c25648f
SilenceFox <cristianplaybr@gmail.com>
2021-03-16 21:56:59 -0300

    ala

---
+++ Assets/Rocket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+
+    public void SetParticleSystemState(bool state)
+    {
+        if (state)
+            particleSystem.Play();
+        else
+            particleSystem.Stop();
+    }
+    
+}
